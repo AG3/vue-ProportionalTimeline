4521c2475fd2ca3b671b8621b9c07a6f3631af04
JiaYi Zhang <bbfag3@gmail.com>
2018-05-07 00:45:00 -0700

    add file i/o

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import web
 import json
+import atexit
 
 urls = (
     '/', 'index',
@@ -7,20 +8,41 @@ urls = (
 )
 
 
+class dataManager:
+    file_path = './data.json'
+    database = []
+    def readFile(self):
+        f = open(self.file_path, 'r')
+        data = json.load(f)
+        return data
+
+    def writeFile(self):
+        f = open(self.file_path, 'w')
+        f.write(json.dumps(self.database))
+
+    def addData(self, e):
+        self.database.append(e)
+
+
+DB = dataManager()
+
 class index:
     def GET(self):
-        render = web.template.render('../../vue-ProportionalTimeline/dist/')
+        render = web.template.render('../VueProject/SclaedTimeline/dist')
         return render.index()
 
 
 class events:
-    def __init__(self):
-        self.data = {'time': 1, 'title': 'hello', 'content': 'xixixi'}
-
     def GET(self):
-        return json.dumps(self.data)
+        return json.dumps(DB.database)
+
+    def POST(self):
+        data = web.data()
+        DB.addData(json.loads(data.decode("utf-8")))
+        return 'OKAY'
 
 
 if __name__ == "__main__":
+    atexit.register(DB.writeFile)
     app = web.application(urls, globals())
     app.run()
